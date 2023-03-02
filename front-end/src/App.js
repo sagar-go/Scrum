64cbcb43249b592862db0d5edcf154338adb22fe
mitesh <miteshkumar862@gmail.com>
2023-03-02 11:23:57 +0530

    front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import User from "./Components/User";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
